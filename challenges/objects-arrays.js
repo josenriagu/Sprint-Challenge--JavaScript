290ef3104ddbd5f1b18a63be31fe15c23f708630
Josemaria <josemarianriagu25@gmail.com>
2019-05-10 10:08:48 +0100

    - Research and use includes method in objects-array.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -83,7 +83,7 @@ console.log(universities.sort())
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
 The resulting contact information should have a space between the first name and the email information like this: 
-Name email@example.com
+Name email@example.com  
 
 Log the result of your new array. */
 const contactInfo = [];
@@ -98,7 +98,7 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
 for (let i = 0; i < graduates.length; i++) {
-  if (graduates[i].university.substring(0, 3) === 'Uni') {
+  if (graduates[i].university.includes('Uni')) {
     uni.push(graduates[i].university)
   }
 }
